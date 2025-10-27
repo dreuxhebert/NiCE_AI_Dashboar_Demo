d42adffb2744fc98aca64f5febae7a29a708ebab
Siddhartha Dhurve <siddhartha.dhurve@nice.com>
2025-10-27 09:51:19 +0530

    Merge main and siddhartha-frontend

--- src/components/add-question-drawer.tsx
+++ src/components/add-question-drawer.tsx
@@ -40,7 +40,7 @@ export function AddQuestionDrawer({ open, onClose }: AddQuestionDialogProps) {
             "questionDescription": "",
             "type": selectedTypes
         }
-        const response = await axios.post('/api/proxy/addQuestion', {
+        const response = await axios.post('/api/proxy/questionSet', {
           model,
         });
         setQuestion("")
